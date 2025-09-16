e701a8450094e2c4acca4feb77ec718b2a9f2622
Parker Woodard <pbw5@students.uwf.edu>
2025-09-16 10:28:12 -0500

    Added conditional checks for part 2. Not functional

--- myshell.cpp
+++ myshell.cpp
@@ -20,14 +20,59 @@ int main (int argc, char *argv[])
     
         if (strcmp(inputLine, "exit") == 0) {
             exit = true;
+            /* terminates the shell; all child processes must be terminated for the shell to close avoiding creation of zombie processes */
         }
         else {
             myParser.getUserInput(inputLine, myParameters);
             if (debugMode == true){
                 myParameters.printParams();
             }
+            
+            if (argumentVector[0] == "cat"){
+                if (!background){
+                    /* displays the source code of the program on the screen */
+                }
+                else{
+                    if (argumentVector[1] == NULL){
+                        /* displays the source code of the program on the screen as below except the output will be displayed in the background causing the prompt of the shell to be mixed with the output of the file*/
+                    }
+                    else{
+                        /* displays the content of the text file testfile.txt on the screen in the background */
+                    }
+                }
+            }
+            else if (argumentVector[0] == "ls" && argumentVector[1] == "-l"){
+                if (outputRedirect == NULL){
+                    /* shows a listing of files in the current directory */
+                }
+                else{
+                    /* writes a listing of files into the text file testfile.txt */
+                }
+            }
+            else if (argumentVector[0] == "./slow" && background){
+                /* runs the program slow from the current working directory in the background */
+            }
+            else if (argumentVector[0] == "grep" && argumentVector[1] == "-i" && argumentVector[2] == "shell"){
+                /* list many lines containing the word shell in the previous file */
+            }
+            else {
+                /* unknown command */
+            }   
         }
     }
     delete [] inputLine;
     return 0;
 }
+
+//ls –l                       shows a listing of files in the current directory
+//ls –l >testfile.txt         writes a listing of files into the text file testfile.txt
+//grep –i shell testfile.txt  list many lines containing the word shell in the previous file
+//cat <myshell.cpp            displays the source code of the program on the screen
+//cat <myshell.cpp &          as above except the output will be displayed in the background causing  
+//                            the prompt of the shell to be mixed with the output of the file
+//cat testfile.txt &          displays the content of the text file testfile.txt on the screen in the
+//                            background
+//./slow &                    runs the program slow from the current working directory in the
+//                            background
+//exit                        terminates the shell; all child processes must be terminated for the shell
+//                            to close avoiding creation of zombie processes
